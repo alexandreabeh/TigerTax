e8b8d97122d55d43e213b73b5473f263d614dce8
Alexandrea Beh <abeh@navexglobal.com>
2016-01-17 23:19:30 -0800

    Proper commit without dlls. Initial framework with SQLite proof of concept.

---
+++ TigerTax/TigerTax/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TigerTax
+{
+    public class Entry
+    {
+        public int EntryId { get; set; }
+        public DateTime Date { get; set; }
+        public String Location { get; set; }
+        public String Description { get; set; }
+        public Double Amount { get; set; }
+        public Double Miles { get; set; }
+        public String Other { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
